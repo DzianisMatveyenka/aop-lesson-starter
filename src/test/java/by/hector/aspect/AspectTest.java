7ca45edcde8d507dd49bd4e8f0ff8a60571645ec
DzianisMatveyenka <dzianis.matveyenka@mail.ru>
2018-06-11 08:48:46 +0300

    init commit

---
+++ src/test/java/by/hector/aspect/AspectTest.java
@@ -0,0 +1,17 @@
+package by.hector.aspect;
+
+import by.hector.config.ApplicationConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ApplicationConfig.class)
+public class AspectTest {
+
+    @Test
+    public void test() {
+
+    }
+}
